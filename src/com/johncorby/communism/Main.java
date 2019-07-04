7b605dc230f773689f4bfea184ee1724170d016a
JohnCorby <williamforgames6@gmail.com>
2019-07-04 11:05:21 -0700

    setting up

--- src/com/johncorby/communism/Main.java
+++ src/com/johncorby/communism/Main.java
@@ -1,4 +1,14 @@
 package com.johncorby.communism;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
 }
